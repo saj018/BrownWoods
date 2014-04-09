c211975c29c65a8410a394ead09684a8ad9aed66
saj018 <vsajinda@hotmail.com>
2014-04-08 21:01:12 -0500

    Added dropdown in the index page
    Added user filter dropdown in the index page

--- protected/views/user/index.php
+++ protected/views/user/index.php
@@ -9,11 +9,15 @@ $this->breadcrumbs=array(
 $this->menu=array(
 	array('label'=>'Create User', 'url'=>array('create')),
 	array('label'=>'Manage User', 'url'=>array('admin')),
+    array('label'=>'My Profile', 'url'=>array('myprofile')),
 );
 ?>
-
+<div class="page-name">
 <h1>Users</h1>
-
+<form id="frmUserType" method="POST">
+<?php if(!(Yii::app()->user->isVendor()))echo "<div>Filter users by user types:</div>" . CHtml::dropDownList("UserTypeID", "", CHtml::listData($typesToLoad, 'UserTypeID', 'UserType'),array('empty' => 'Select a user type','onChange' => 'submit','submit' => array('user/filterusersbyusertype'), 'options'=>array($filteredType=>array('selected'=>true))))?><div></div>
+</form>
+</div>
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
